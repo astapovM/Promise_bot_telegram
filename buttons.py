46eb4d748321f78a9d3fbf5dde6192138068c3d1
Господин Ведущий <sustaff@mail.ru>
2022-08-26 19:33:48 +0300

    Кнопки

---
+++ buttons.py
@@ -0,0 +1,14 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+b1 = KeyboardButton("Дать обещание")
+b2 = KeyboardButton("Проверить обещание")
+b3 = KeyboardButton("Удалить обещание")
+b4 = KeyboardButton("Выполнил")
+b5 = KeyboardButton("Еще нет")
+b6 = KeyboardButton("Календарь")
+
+start_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(b6)
+promise_button=ReplyKeyboardMarkup(resize_keyboard=True).add(b1)
+check_promise_button = ReplyKeyboardMarkup(resize_keyboard=True).add(b2)
+delete_promise_button = ReplyKeyboardMarkup(resize_keyboard=True).add(b3)
+promise_answer_button = ReplyKeyboardMarkup(resize_keyboard=True).add(b4,b5)
