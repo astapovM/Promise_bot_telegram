0c61e649a5649980a1e2dd6c3e82d9eb141e9071
Господин Ведущий <sustaff@mail.ru>
2022-09-03 14:24:58 +0300

    Добавлена проверка электронной почты

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import asyncio
 import datetime
+import logging
 import random
+import re
 
+import aioschedule
 from aiogram import Bot, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher, FSMContext
@@ -13,6 +16,16 @@ import buttons
 import db_admin
 import remember_list
 from config import token
+from mail_sendler import send_email
+
+email = "me@host.com"
+
+pattern = r"^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$"
+
+logging.basicConfig(filename='log.txt', filemode='w',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S', encoding='UTF-8', level=logging.DEBUG)
+logging.info("Log started")
 
 bot = Bot(token=token)
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -24,6 +37,7 @@ now = datetime.datetime.now()
 class Promises(StatesGroup):
     promis_text = State()
     promis_date = State()
+    promis_email = State()
 
 
 @dp.message_handler(commands=['start'])
@@ -57,6 +71,7 @@ async def answer_text(message: types.Message, state: FSMContext):
         data["user_id"] = user_id
         data["user_name"] = user_name
         data["promise"] = message.text
+        await message.delete()
 
     await state.reset_state(with_data=False)
     await message.answer("Хорошо.Теперь введи дату дедлайна", reply_markup=await SimpleCalendar().start_calendar())
@@ -67,30 +82,46 @@ async def answer_date(call: types.CallbackQuery, callback_data: dict, state: FSM
     selected, date = await SimpleCalendar().process_selection(call, callback_data)
     if selected:
         await Promises.promis_date.set()
-        await state.update_data(deadline=f'{date.strftime("%d.%m.%Y")}')
+        await state.update_data(deadline=f'{date.strftime("%Y.%m.%d")}')
         data = await state.get_data()
         await db_admin.sql_add(data)
         await state.finish()
-        if db_admin.sql_deadline(call.from_user.id)[0] < now.strftime("%d.%m.%Y"):
+        if db_admin.sql_deadline(call.from_user.id)[0] < now.strftime("%Y.%m.%d"):
             await bot.send_message(call.from_user.id, "Вы выбрали уже прошедшее время.Обещание не было создано")
             db_admin.sql_delete(call.from_user.id)
         else:
             await bot.send_message(call.from_user.id, f'Ты пообещал {data["promise"]} до {data["deadline"]}')
-            await bot.send_message(call.from_user.id, "Буду переодически тебе напоминать об этом")
-        while True:
-            await asyncio.sleep(random.randint(121, 241))
-            db_admin.check_user(call.from_user.id)
-            db_admin.sql_deadline(call.from_user.id)
+            await bot.send_message(call.from_user.id, "Но кому ты даешь это обещание ?",
+                                   reply_markup=buttons.promise_email)
 
-            if db_admin.check_user(call.from_user.id) != None and db_admin.sql_deadline(call.from_user.id)[0] > now.strftime("%d.%m.%Y"):
-                text = random.choice(remember_list.spisok)
-                await bot.send_message(call.from_user.id, text)
-                print(f"Сообщение юзеру {call.from_user.id} доставлено!")
 
-            else:
-                await bot.send_message(call.from_user.id, "Создайте новое обещание командой /start")
-                break
+@dp.message_handler(text="Себе")
+async def answer_sebe(message: types.Message):
+    await message.answer("Ок.Буду переодически тебе напоминать об этом", reply_markup=types.ReplyKeyboardRemove())
+    await message.answer("Просто введи /start ,  для дальнейшего общения со мной")
 
+
+@dp.message_handler(text="Другому человеку")
+async def answer_another(message: types.Message):
+    await message.answer("Тогда введите его email адрес", reply_markup=types.ReplyKeyboardRemove())
+    await Promises.promis_email.set()
+
+
+@dp.message_handler(state=Promises.promis_email)
+async def answer_email(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['email'] = message.text
+        if "@" not in message.text:
+            await message.answer("Введите адрес почты в верном формате (lohpidor@mail.ru)")
+            await Promises.promis_email.set()
+        else:
+            await db_admin.sql_add_email(data, message.from_user.id)
+            await state.finish()
+            await message.delete()
+            await message.answer(
+                f"Почта записана.Если ты не выполнишь своё обещание до {db_admin.promise_check(message.from_user.id)[1]} человеку придет письмо"
+                f" с рассказом о том, как ты его обманул и не выполнил обещанного.")
+            await message.answer("Просто введи /start ,  для дальнейшего общения со мной")
 
 
 
@@ -98,7 +129,7 @@ async def answer_date(call: types.CallbackQuery, callback_data: dict, state: FSM
 async def check_promise(message: types.Message):
     db_admin.promise_check(message.from_user.id)
     await message.answer(
-        f"Ты обещал {db_admin.promise_check(message.from_user.id)[0]} до {db_admin.promise_check(message.from_user.id)[1]}")
+        f"Ты обещал {db_admin.check_promise(message.from_user.id)[0]} до {db_admin.promise_check(message.from_user.id)[1]}")
     await message.answer("Ты выполнил это обещание?", reply_markup=buttons.promise_answer_button)
 
 
@@ -118,35 +149,70 @@ async def delete_promise(message: types.Message):
 
 @dp.message_handler(text="Еще нет")
 async def answer_no(message: types.Message):
-    await bot.send_photo(message.from_user.id, "https://oir.mobi/uploads/posts/2019-12/1575947618_3-3.jpg",
+    await bot.send_photo(message.from_user.id, "https://raritem.ru/wp-content/uploads/2021/12/thing.jpg",
                          reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(commands=['sendall'])
 async def sendall(message: types.Message):
-    if message.chat.type == "private":
-        if message.from_user.id == 293427068:
+    if message.from_user.id == 293427068:
+        while True:
+            text = random.choice(remember_list.spisok)
+            users = db_admin.all_users()
+            for user in users:
+                await bot.send_message(user[0], text)
+                print(f"Сообщение юзеру {user} доставлено")
+            await asyncio.sleep(random.randint(120, 240))
 
-            while True:
 
-                text = random.choice(remember_list.spisok)
-                users = db_admin.all_users()
-                for user in users:
-                    await bot.send_message(user[0], text)
-                    print(f"Сообщение юзеру {user} доставлено")
-                await asyncio.sleep(random.randint(120, 240))
+@dp.message_handler(commands=['spam'])
+async def spam(message):
+    for deadline in db_admin.check_info():
+        if deadline[1] <= now.strftime("%Y.%m.%d"):
+            await bot.send_message(deadline[0],
+                                   "Время вышло и ты не выполнил обещание.Отныне ты официально чорт.Поздравляем  🥳 🥳  🥳  ")
+
+            for mail in db_admin.check_email():
+                if mail[0] != None:
+                    print(mail[0])
+                    await send_email(f"{mail[0]}")
+                    db_admin.sql_delete(deadline[0])
+
+        else:
+            text = random.choice(remember_list.spisok)
+            await bot.send_message(deadline[0], text)
+            print(f"Сообщение юзеру {deadline[0]} доставлено в {now.strftime('%H:%M  %d.%m.%Y')}!")
+
+
+async def scheduler():
+    aioschedule.every().day.at("12:35").do(spam, "message")
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(10)
+
+
+async def on_startup(_):
+    asyncio.create_task(scheduler())
 
 
 @dp.message_handler(commands=['sendone'])
 async def sendone(message: types.Message):
     if message.from_user.id == 293427068:
-        text = ("Напоминаю, что ты обещал Помочь лучшему ученику с работой до 01.09.2022.Иначе ебасосина под угрозой отлёта)))")
+        text = ("Ну шо ты , маладой шершень.Не забыл еще что обещал?")
         user_volos = 161611465
         user_admin = 293427068
         user_star = 87241346
-        await bot.send_message(, text)
-        print(f"Сообщение юзеру {} доставлено")
+        user_vovan = 881052206
+        user_vlad = 343308620
+        await bot.send_photo(user_star, "https://memepedia.ru/wp-content/uploads/2017/07/1429875891_1642837939.png")
+        print(f"Сообщение юзеру {user_star} доставлено в {now.strftime('%H:%M  %d.%m.%Y')}!")
+
+
+@dp.message_handler()
+async def command_not_found(message: types.Message):
+    await message.delete()
+    await message.answer(f"Команда {message.message_id} не найдена")
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
