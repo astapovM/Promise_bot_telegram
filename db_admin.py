6bb4e48270b938c98e4e2cdde22a1247395b34d8
AstapovM <sustaff@mail.ru>
2022-09-05 18:16:12 +0300

    Тест загрузки

--- db_admin.py
+++ db_admin.py
@@ -10,17 +10,41 @@ def sql_start():
 
 
 def promise_check(user_id):
-    cur.execute(f"SELECT promise, deadline FROM users where user_id ={user_id}")
-    results= cur.fetchall()
+    cur.execute(f"SELECT user_id, deadline FROM users ")
+    results = cur.fetchall()
     for i in results:
-        promise = str(i[0])
+        user = str(i[0])
         deadline = str(i[1])
-    return promise, deadline
+    return user, deadline
+
+
+def check_info():
+    cur.execute(f"SELECT user_id, deadline FROM users ")
+    results = cur.fetchall()
+    return results
+
+
+def check_promise(user_id):
+    cur.execute(f"SELECT promise FROM users WHERE user_id = {user_id}")
+    result = cur.fetchone()
+    return result
+
+
+def check_email():
+    cur.execute(f"SELECT email from users")
+    result = cur.fetchall()
+    return result
+
 
 async def sql_add(data):
-    cur.execute("INSERT INTO users VALUES (?, ?, ?, ?)", [data['user_id'], data['user_name'], data['promise'], data['deadline']])
+    cur.execute("INSERT OR IGNORE INTO users (user_id, user_name, promise, deadline) VALUES (?, ?, ?, ?)",
+                [data['user_id'], data['user_name'], data['promise'], data['deadline']])
     base.commit()
 
+
+async def sql_add_email(data, user_id):
+    cur.execute(f"UPDATE users SET email = '{data['email']}' WHERE user_id = {user_id} ")
+    base.commit()
 
 
 def all_users():
@@ -28,15 +52,17 @@ def all_users():
     return cur.fetchall()
 
 
+def sql_deadline(user_id):
+    cur.execute(f"SELECT deadline from users where user_id = {user_id}")
+    return cur.fetchone()
+
 
 def sql_delete(user_id):
     cur.execute(f"DELETE from users WHERE user_id ={user_id}")
     base.commit()
 
 
-
-
 def check_user(user_id):
     cur.execute(f"SELECT * FROM users where user_id ={user_id}")
-    res=cur.fetchone()
+    res = cur.fetchone()
     return res
