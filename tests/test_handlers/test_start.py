d2acab01cc0c1e0a20d66f850aad62ce88fff716
AstapovM <sustaff@mail.ru>
2022-09-08 16:12:24 +0300

    асинкмок

--- tests/test_handlers/test_start.py
+++ tests/test_handlers/test_start.py
@@ -1,11 +1,13 @@
 import pytest
 from unittest.mock import AsyncMock
+
+import buttons
 from main import process_start_command
 
 
 @pytest.mark.asyncio
 async def test_start_handler():
     message = AsyncMock()
-    await process_start_command()
+    await process_start_command(message)
 
-    message.answer.assert_called_with()
+    message.answer.assert_called_with(reply_markup=buttons.check_promise_button)
